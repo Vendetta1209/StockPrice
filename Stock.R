9979ee4919dba89228719d4d2d9fac910df2dbb1
masaovl <masao9090@gmail.com>
2018-07-18 12:34:45 +0900

    データベースのテーブル名変更

--- Stock.R
+++ Stock.R
@@ -2,6 +2,7 @@ library(RODBC)
 library(XML)
 library(quantmod)
 library(stringr)
+library(dplyr)
 
 #ODBC
 db <- odbcConnect("rtest", uid="test", pwd="test")
@@ -24,17 +25,17 @@ insertSqlData <- function(ticker_id, stock_type){
   for (i in 1:nrow(tmp)){
     switch(stock_type,
            #Open
-           "1" = sqlQuery(db, str_c("INSERT INTO dbo.OpenTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,2],"')")),
+           "1" = sqlQuery(db, str_c("INSERT INTO dbo.OpenPrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,2],"')")),
            #High
-           "2" = sqlQuery(db, str_c("INSERT INTO dbo.HighTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,3],"')")),
+           "2" = sqlQuery(db, str_c("INSERT INTO dbo.HighPrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,3],"')")),
            #Low
-           "3" = sqlQuery(db, str_c("INSERT INTO dbo.LowTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,4],"')")),
+           "3" = sqlQuery(db, str_c("INSERT INTO dbo.LowPrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,4],"')")),
            #Close
-           "4" = sqlQuery(db, str_c("INSERT INTO dbo.CloseTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,5],"')")),
+           "4" = sqlQuery(db, str_c("INSERT INTO dbo.ClosePrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,5],"')")),
            #Volume
-           "5" = sqlQuery(db, str_c("INSERT INTO dbo.VolumeTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,6],"')")),
+           "5" = sqlQuery(db, str_c("INSERT INTO dbo.Volume VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,6],"')")),
            #Adjusted
-           "6" = sqlQuery(db, str_c("INSERT INTO dbo.AdjustedTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,7],"')"))
+           "6" = sqlQuery(db, str_c("INSERT INTO dbo.AdjustedPrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,7],"')"))
     )
   }
 }
@@ -44,17 +45,17 @@ insertSqlDataAll <- function(ticker_id){
   tmp <- getStock(ticker_id) %>% toDataFrame()
   for (i in 1:nrow(tmp)){
     #Open
-    sqlQuery(db, str_c("INSERT INTO dbo.OpenTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,2],"')"))
+    sqlQuery(db, str_c("INSERT INTO dbo.OpenPrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,2],"')"))
     #High
-    sqlQuery(db, str_c("INSERT INTO dbo.HighTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,3],"')"))
+    sqlQuery(db, str_c("INSERT INTO dbo.HighPrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,3],"')"))
     #Low
-    sqlQuery(db, str_c("INSERT INTO dbo.LowTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,4],"')"))
+    sqlQuery(db, str_c("INSERT INTO dbo.LowPrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,4],"')"))
     #Close
-    sqlQuery(db, str_c("INSERT INTO dbo.CloseTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,5],"')"))
+    sqlQuery(db, str_c("INSERT INTO dbo.ClosePrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,5],"')"))
     #Volume
-    sqlQuery(db, str_c("INSERT INTO dbo.VolumeTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,6],"')"))
+    sqlQuery(db, str_c("INSERT INTO dbo.Volume VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,6],"')"))
     #Adjusted
-    sqlQuery(db, str_c("INSERT INTO dbo.AdjustedTable VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,7],"')"))
+    sqlQuery(db, str_c("INSERT INTO dbo.AdjustedPrice VALUES ('",ticker_id,"','",tmp[i,1],"','",tmp[i,7],"')"))
     
   }
 }
